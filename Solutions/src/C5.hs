6d8ec8700d2eeec16c5a2f9e900dcef0990f97a9
Adam Piper <adam@ahri.net>
2017-11-02 21:58:51 +0000

    Chapter 5.

--- Solutions/src/C5.hs
+++ Solutions/src/C5.hs
@@ -1,4 +1,47 @@
 module C5 where
 
 c5 :: IO ()
-c5 = pure ()
+c5 = do
+    putStrLn "5.4 Type Arguments"
+    print $ 3.5 `plus` 2.7 -- seems to work fine without NoMonomorphismRestriction
+    where
+        plus = (+)
+        c5_ta_1_h :: Int -> Char
+        c5_ta_1_h = g . f
+            where
+                f :: Int -> String
+                f = undefined
+                g :: String -> Char
+                g = undefined
+
+        c5_ta_2_e :: A -> C
+        c5_ta_2_e = w . q
+            where
+                q :: A -> B
+                q = undefined
+                w :: B -> C
+                w = undefined
+
+        c5_ta_3_xform :: (X, Y) -> (Z, Z)
+        c5_ta_3_xform (x, y) = (xz x, yz y)
+            where
+                xz :: X -> Z
+                xz = undefined
+                yz :: Y -> Z
+                yz = undefined
+
+        c5_ta_4_munge :: (x -> y)
+                      -> (y -> (w, z))
+                      -> x
+                      -> w
+        c5_ta_4_munge x_y y_wz = fst . y_wz . x_y
+
+
+
+data A
+data B
+data C
+
+data X
+data Y
+data Z
