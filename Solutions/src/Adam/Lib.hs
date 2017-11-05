459320238ba9dbc8431b7fa0a03217b8fed3915d
Adam Piper <adam@ahri.net>
2017-11-05 19:10:37 +0000

    Start chapter 9.

--- Solutions/src/Adam/Lib.hs
+++ Solutions/src/Adam/Lib.hs
@@ -7,9 +7,10 @@ import           C05
 import           C06
 import           C07
 import           C08
+import           C09
 
 chapters :: IO ()
-chapters = output [c02, c03, c04, c05, c06, c07, c08] 2
+chapters = output [c02, c03, c04, c05, c06, c07, c08, c09] 2
 
 output :: [IO ()] -> Integer -> IO ()
 output [] _     = pure ()
