c0559f44d0fbf1ef21997dfdb4e722ac44a80f87
chris-bacon <chris.bacon@geekology.ltd.uk>
2017-11-20 21:32:01 +0000

    q1

--- Solutions/src/Chris/ChrisC10.hs
+++ Solutions/src/Chris/ChrisC10.hs
@@ -1,5 +1,12 @@
 module ChrisC10 where
 
-testc10 :: IO ()
-testc10 = do
-    print "test"
+testC10 :: IO ()
+testC10 = do
+    print $ foldr (*) 1 [1..5] == foldl (*) 1 [1..5]
+    print $ foldr (*) 1 [1..5] == foldl (flip (*)) 1 [1..5]
+
+-- foldr (*) 1 [1..5] -- 120
+--      will return the same result as which of the following:
+-- a) flip (*) 1 [1..5]
+-- b) foldl (flip (*)) 1 [1..5] - 120
+-- c) foldl (*) 1 [1..5] -- 120
