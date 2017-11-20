bf2f24def919ae7dd87e83b047b4ef710cb9698c
chris-bacon <chris.bacon@geekology.ltd.uk>
2017-11-20 21:33:06 +0000

    q1

--- Solutions/src/Chris/ChrisC10.hs
+++ Solutions/src/Chris/ChrisC10.hs
@@ -4,6 +4,8 @@ testC10 :: IO ()
 testC10 = do
     print $ foldr (*) 1 [1..5] == foldl (*) 1 [1..5]
     print $ foldr (*) 1 [1..5] == foldl (flip (*)) 1 [1..5]
+    print "foldr, but not foldl, associates to the right"
+    print "catamorphisms reduce structure"
 
 -- foldr (*) 1 [1..5] -- 120
 --      will return the same result as which of the following:
